b4246bdbe9862d2fb7a0575adff8281fdf14f222
Michael Lindemuth <mlindemu@crow.marine.usf.edu>
2015-02-18 22:19:41 +0000

    NavBar now works

--- public/javascripts/components/NavBar.jsx
+++ public/javascripts/components/NavBar.jsx
@@ -2,16 +2,20 @@ var React = require('react');
 var MapPanelMixin = require('./MapPanelMixin.jsx');
 
 var LayerItem = React.createClass({
+    onClick: function(e){
+        e.stopPropagation();
+    },
+
     onChange: function(e){
         console.log(e);
     },
 
     render: function(){
         return (
-            <li>
+            <li onClick={this.onClick}>
                 <div className="checkbox">
                     <label>
-                        <input type="checkbox" onChange={this.onChange}/> {this.props.layer.name}
+                        <input type="checkbox" onChange={this.onChange} /> {this.props.layer.name}
                     </label>
                 </div>
             </li>
@@ -21,15 +25,31 @@ var LayerItem = React.createClass({
 
 var NavBarDropDown = React.createClass({
     render: function(){
-        var sourceLayers = this.props.layers.map(function(layer){
-            return (
-                <LayerItem key={layer.uri} layer={layer} />
-            );
+        var categorizedLayers = _.groupBy(this.props.layers, function(layer){
+            return layer.station_type;
         });
+
+        var sourceLayers = [];
+        _.each(categorizedLayers, function(layers, category){
+            var layerItems = layers.map(function(layer){
+                return (
+                    <LayerItem key={layer.uri} layer={layer} />
+                );
+            });
+            sourceLayers.push(
+                <div key={this.props.provider.short_name + "." + category}>
+                    <label className="layer-group-header">{category}</label>
+                    <ul className="layer-group-ul">
+                        {layerItems}
+                    </ul>
+                </div>
+            );
+        }, this);
+
         return (
             <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{this.props.provider.short_name}<span className="caret"></span></a>
-                <ul className="dropdown-menu" role="menu">
+                <ul className="dropdown-menu layers-ul" role="menu">
                     {sourceLayers}
                 </ul>
             </li>
@@ -41,37 +61,38 @@ var NavBar = React.createClass({
     mixins: [MapPanelMixin],
 
     render: function(){
-        var dropDowns = null;
+        var dropDowns = [];
         if(this.props.sources){
             dropDowns = this.props.sources.map(function(source){
-                var blah = source;
                 return (
-                    <NavBarDropDown key={source.provider.uri} provider={source.provider} layers={source.layers} />
+                    <NavBarDropDown key={source.provider.url} provider={source.provider} layers={source.layers} />
                 );
             });
         }
 
         return (
             <div id="nav-region">
-                <nav className="navbar navbar-default">
-                    <div className="container-fluid">
-                      <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                          <span className="sr-only">Toggle navigation</span>
-                          <span className="icon-bar"></span>
-                          <span className="icon-bar"></span>
-                          <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand" href="#">Layers</a>
-                      </div>
-                
-                      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav">
-                            {dropDowns}
-                        </ul>
-                      </div>
-                    </div>
-                </nav>
+                <div id="nav-container">
+                    <nav className="navbar navbar-default">
+                        <div className="container-fluid">
+                          <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                              <span className="sr-only">Toggle navigation</span>
+                              <span className="icon-bar"></span>
+                              <span className="icon-bar"></span>
+                              <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">Layers</a>
+                          </div>
+                    
+                          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                {dropDowns}
+                            </ul>
+                          </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
         );
     }
